817a950e9d5de9a4753875361b4b536b9edfd233
Oleg <olijenius@gmail.com>
2020-04-10 22:10:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 // HERTML to HTML
 
-echo 
+function htmlToHer($hertml) {
     str_ireplace('</her>', '</a>',
         str_ireplace('<\her/>', '">',  
-            str_ireplace('<her\>', '<a href="', "Это текст <her\>http://ingello.com<\her/>Это кнопка</her>")
+            str_ireplace('<her\>', '<a href="', $hertml)
         )
     );
+}
+
+echo htmlToHer("Привет мир! Это текст, <her\>http://ingello.com<\her/>а это ссылка кнопка</her>");
